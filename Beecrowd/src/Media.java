e8ef314b799d49ae7f162edda2d72bc94515501d
Lucas Gabriel <105455415+Lucasgabdom@users.noreply.github.com>
2025-06-27 07:44:36 -0300

    Add files via upload

---
+++ Beecrowd/src/Media.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double calculando = ((a * 2) + (b * 3) + (c * 5));
+        double media = calculando / 10;
+
+        System.out.printf("MEDIA = %.1f\n", media);
+    }
+}
